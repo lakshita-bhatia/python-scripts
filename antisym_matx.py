eecae1599febd5219ee6527d813e7af4ac09dae2
lakshita-bhatia <lakshitabhatia2014@gmail.com>
2017-01-14 08:17:02 +0530

    Symmetric Square returns True if the given square is antisymmetric and false otherwise

---
+++ antisym_matx.py
@@ -0,0 +1,36 @@
+# Symmetric Square returns True
+# if the given square is antisymmetric and False otherwise.
+# An nxn square is called antisymmetric if A[i][j]=-A[j][i]
+# for each i=0,1,...,n-1 and for each j=0,1,...,n-1.
+
+def antisymmetric(A):
+    o = len(A)
+    for i in range(o):
+        for j in range(o):
+            if A[i][j] != -A[j][i]:
+                return False
+    return True
+
+
+
+# Test Cases:
+
+print antisymmetric([[0, 1, 2],
+                     [-1, 0, 3],
+                     [-2, -3, 0]])
+#>>> True
+
+print antisymmetric([[0, 0, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]])
+#>>> True
+
+print antisymmetric([[0, 1, 2],
+                     [-1, 0, -2],
+                     [2, 2,  3]])
+#>>> False
+
+print antisymmetric([[1, 2, 5],
+                     [0, 1, -9],
+                     [0, 0, 1]])
+#>>> False
