496a430c55f58b02275ef12041660f75bc16f478
lakshita-bhatia <lakshitabhatia2014@gmail.com>
2017-01-25 11:15:54 +0530

    pdf to mp3 convertor added

---
+++ pdf2mp3.py
@@ -0,0 +1,31 @@
+import os,sys
+import string
+
+# get filenname
+ifpdf=1
+filename_inp = sys.argv[1]
+
+if filename_inp[-4:]=='.dat' or filename_inp[-4:]=='.txt':
+    ifpdf=0
+
+if filename_inp[-4:]=='.pdf' or filename_inp[-4:]=='.dat' or filename_inp[-4:]=='.txt':
+    filename = filename_inp[:-4]
+    if os.path.isfile(str(filename_inp)):
+        if ifpdf:
+            print 'converting pdf to ascii...'
+            os.popen('pdftotext ' + str(filename) + '.pdf ' + str(filename) + '.txt')
+        print 'start converting ascii to wav...'
+        os.popen('cat ' + str(filename) + '.txt\
+               |sed \'s/[^a-zA-Z .,!?]//g\'|text2wave -o ' + str(filename) + '.wav')
+        print 'converting to mp3...'
+        os.popen('lame -f ' + str(filename) + '.wav ' + str(filename) + '.mp3')
+        os.popen('rm -f ' + str(filename) + '.wav')
+        print 'finished. don\'t forget to delete txt file if you don\'t need it'
+    else:
+        print '*** file does not exist ***'
+else:
+    print '*** please give extension of your file (.dat, .txt or .pdf)'
+    print '*** or your file called: ' + str(filename_inp) + '\n*** does not exist'
+   
+
+# that's it. have fun!!! :)
